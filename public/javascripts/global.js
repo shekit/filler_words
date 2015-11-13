8655334a0fe8e95fcb66302ced1203edb4b78035
shekit <as7790@nyu.edu>
2015-11-13 11:17:41 -0500

    web speech placeholders

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
 	console.log("ready")
+
+	var recognizing = false;
+
+	if(('webkitSpeechRecognition' in window)){
+		var recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
+		recognition.interimResults = true;
+		console.log("have web speech!")
+
+		recognition.onstart = function(){
+			console.log("starteddddd")
+		}
+
+		recognition.onerror = function(){
+			console.log("errrorrr")
+		}
+
+		recognition.onend = function(){
+			console.log("stopppedd")
+		}
+
+		recognition.onresult = function(event){
+			console.log("got some resullttt")
+		}
+
+	}
+
+	$("#record").on('click', function(event){
+		console.log("start recognizing");
+
+	})
+
 })
